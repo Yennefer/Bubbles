12109b5eaf781f7f9dce22e01dccdcceaf108d10
Alena Kabardinova <zakomoldinaav@gmail.com>
2018-02-13 00:36:22 +0300

    Add bubble cost and popping feature

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour {
 
+	[SerializeField]
 	private float speed = 0.5F;
+	[SerializeField]
+	private int cost = 10;
 		
 	private void Update () {
 		gameObject.transform.Translate(Vector2.up * speed * Time.deltaTime);
+	}
+
+	public int Pop() {
+		Destroy(gameObject);
+		return cost;
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,20 @@ public class GameManager : MonoBehaviour {
 		StartGame();
 	}
 
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+			if (hit.collider) {
+				Bubble bubble = hit.collider.gameObject.GetComponent<Bubble>();
+
+				if (bubble) {
+    				Debug.Log("I get " + bubble.Pop() + " points");
+				}	
+			}
+		}	
+	}
+
 	private void StartGame() {
 		spawner.StartSpawn();
 	}
